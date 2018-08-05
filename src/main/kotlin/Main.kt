f549d007a8452c23e0e8f54aeaeb58d10697fac9
Unknown <syedyusha@gmail.com>
2018-08-05 13:54:29 +0200

    Statement and IncompleteStatement classes ready. Their use is described in the class file. Inheritance power of OOP is being used.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,2 @@
 fun main(args: Array<String>) {
-    val statement1 = OntoStatement("H_Yusha","isIn",2)
-
-//    println("Incomplete Statement==> The subject is: ${statement1.subject_OWLindividual}, the verb is: ${statement1.verb_OWLproperty}")
 }
--- src/main/kotlin/OntoStatement.kt
+++ src/main/kotlin/OntoStatement.kt
@@ -5,26 +5,44 @@ import java.sql.Timestamp
  * A full statement is made up of the structure: 'subject' 'verb' 'object', as in English
  * analogous to the structure: 'Individual' 'Object property' 'Individual' OR 'Individual' 'Data property' 'Data(of some type)', in OWL.
  *
- * OntoStatement is a child class of IncompleteOntoStatement (Inheritance)
+ * OntoStatement is (the child class) of IncompleteOntoStatement (the parent class).
  *
  * The idea is,
- * to use <code>IncompleteOntoStatements</code>, to find inference from them, and
- * to use <code>OntoStatements</code>, to set statements in an Onto, check presence in an Onto, delete from Onto.
+ * to use <code>IncompleteOntoStatement</code>, when finding inference, and
+ * to use <code>OntoStatement</code>, when setting statements in an Onto, checking presence in an Onto, deleting from Onto.
  */
 
-open class IncompleteOntoStatement(var OWLindividualAsSubject: String, var OWLpropertyAsVerb: String)
+open class IncompleteOntoStatement(var subjectAsOwlIndividual: String, var verbAsOwlProperty: String)
 
 class OntoStatement: IncompleteOntoStatement {
-    lateinit var OWLindividualAsObject: String
-    lateinit var OWLtimestampDataAsObject: Timestamp
-    var OWLintegerDataataAsObject: Int = 0
-    var OWLbooleanDataataAsObject: Boolean = false
+    lateinit var objectAsOwlIndividual: String
+    lateinit var objectAsOwlTimestampData: Timestamp
+    var objectAsOwlIntegerData: Int = 0
+    var objectAsOwlBooleanData: Boolean = false
 
-    //Can be used for OWL Object-properties
-    constructor(OWLindividualAsSubject: String, OWLpropertyAsVerb: String, OWLindividualAsObject: String) : super(OWLindividualAsSubject, OWLpropertyAsVerb)
+    /**
+     * Constructor can be used when statement's 'verb' is an owlObjectProperty.
+     */
+    constructor(subjectAsOwlIndividual: String, verbAsOwlProperty: String, objectAsOwlIndividual: String) : super(subjectAsOwlIndividual, verbAsOwlProperty) {
+        this.objectAsOwlIndividual = objectAsOwlIndividual
+    }
 
-    //Can be used for OWL Data-properties
-    constructor(OWLindividualAsSubject: String, OWLpropertyAsVerb: String, OWLtimestampDataAsObject: Timestamp) : super(OWLindividualAsSubject, OWLpropertyAsVerb)
-    constructor(OWLindividualAsSubject: String, OWLpropertyAsVerb: String, OWLintegerDataataAsObject: Int) : super(OWLindividualAsSubject, OWLpropertyAsVerb)
-    constructor(OWLindividualAsSubject: String, OWLpropertyAsVerb: String, OWLbooleanDataataAsObject: Boolean) : super(OWLindividualAsSubject, OWLpropertyAsVerb)
+    /**
+     * Constructor can be used when statement's 'verb' is an owlDataProperty when data type is a Timestamp.
+     */
+    constructor(subjectAsOwlIndividual: String, verbAsOwlProperty: String, objectAsOwlTimestampData: Timestamp) : super(subjectAsOwlIndividual, verbAsOwlProperty) {
+        this.objectAsOwlTimestampData = objectAsOwlTimestampData
+    }
+    /**
+     * Constructor can be used when statement's 'verb' is an owlDataProperty when data type is an Integer.
+     */
+    constructor(subjectAsOwlIndividual: String, verbAsOwlProperty: String, objectAsOwlIntegerData: Int) : super(subjectAsOwlIndividual, verbAsOwlProperty) {
+        this.objectAsOwlIntegerData = objectAsOwlIntegerData
+    }
+    /**
+     * Constructor can be used when statement's 'verb' is an owlDataProperty when data type is a Boolean.
+     */
+    constructor(subjectAsOwlIndividual: String, verbAsOwlProperty: String, objectAsOwlBooleanData: Boolean) : super(subjectAsOwlIndividual, verbAsOwlProperty) {
+        this.objectAsOwlBooleanData = objectAsOwlBooleanData
+    }
 }
