6beb4fc4b29672ac3ba10a9bb4f54c8a57223006
Unknown <syedyusha@gmail.com>
2018-11-05 00:09:34 +0100

    Developed new Ontologies: Place, KitchenActivity, LivingRoomActivity

--- src/main/kotlin/OntologyNetworkBuilder.kt
+++ src/main/kotlin/OntologyNetworkBuilder.kt
@@ -1,5 +1,5 @@
 /**
- A network of ontologies is an attempt at computationally-scalable reasoning using formal logic.
+ A network of ontologies is an attempt at computationally-scalable reasoning using formal logic for human activity recognition.
  Ontologies are based on a system of formal logic called Description-logic. Protege is the editor used to create such ontologies.
 
  For a clear understanding of above sentences, explore these links:
